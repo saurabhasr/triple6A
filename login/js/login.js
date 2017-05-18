5586fab3f98388a785cc4a0af260f8ff03c89190
sikka726 <dipaksikka@gmail.com>
2017-05-18 15:05:56 +0530

    new color combination

--- login/js/login.js
+++ login/js/login.js
@@ -7,15 +7,29 @@ app.controller('myCtrl','$http','loginValidation', function($scope){
     $scope.login = false;
 
     $scope.checkLogin = function(){
-        
-        var user= {
-            userId : $scope.username,
-            password: $scope.password
+       
+         if(
+            ($scope.username=="admin" && $scope.password == "password")||
+            ($scope.username=="sanjana" && $scope.password == "password")
+        ){
+            $scope.login = true;
+            localStorage.setItem('auth', 'true');
+            location.href = "../../application/content/index.html"
         }
-        
-        //calling post method
-        loginValidation.validateUser(user);                        
+        else{
+            $scope.login = false;
+            localStorage.setItem('auth', 'false');
+            sweetAlert("", "Invalid Username or Password", "error");
+        }
     }
+//        var user= {
+//            userId : $scope.username,
+//            password: $scope.password
+//        }
+//        
+//        //calling post method
+//        loginValidation.validateUser(user);                        
+//    }
 
 
 });
